0f62a9c5d369db168c6c31c46ccefe78f4df3e59
Collins Ogera <ogeracollins@gmail.com>
2023-08-11 08:07:54 +0000

    State class

---
+++ models/state.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+States class deninition
+"""
+from models.base_model import BaseModel
+
+class State(BaseModel):
+    """
+    Represents a state class
+    Attributes:
+        name(str): Name of the state
+    """
+
+    name = ""
