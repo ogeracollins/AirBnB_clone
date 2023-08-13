46972971a12ea29ea7c0dd2e6826ceb39e1fe42d
Collins Ogera <ogeracollins@gmail.com>
2023-08-13 17:39:50 +0000

    Test cases

---
+++ tests/test_models/test_state.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Test suits for state
+"""
+import os
+import models
+import unittest
+from datetime import datetime
+from models.base_model import BaseModel
+
+
+class TestState(unittest.TestCase):
+    """
+    Tests for state
+    """
+
+    def test_name(self):
+        """
+        Tests for name inputs
+        """
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
